5e4111bffd838194f10c190fc07ffbf5fde4b853
akalayci18 <72552136+akalayci18@users.noreply.github.com>
2020-12-24 14:32:00 +0300

    Post-app Notification

--- Assets/Scripts/DataStructure/AnnoData.cs
+++ Assets/Scripts/DataStructure/AnnoData.cs
@@ -27,3 +27,4 @@ public class AnnoData : DataRoot
 
     }
 }
+
---
+++ Assets/Scripts/NotificationAndroid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Notifications.Android;
+
+public class NotificationAndroid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var channel = new AndroidNotificationChannel()
+        {
+            Id = "channel_id",
+            Name = "Default Channel",
+            Importance = Importance.Default,
+            Description = "Generic notifications",
+        };
+        AndroidNotificationCenter.RegisterNotificationChannel(channel);
+
+        var notification = new AndroidNotification();
+        notification.Title = "PATİ";
+        notification.Text = "Minik doslarımızın sana her zaman ihtiyaçları olduğunu unutma!";
+        notification.FireTime = System.DateTime.Now.AddSeconds(10);
+
+        AndroidNotificationCenter.SendNotification(notification, "channel_id");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
