acf7c2e79e9565c14ee452f7785a19b956f70568
akalayci18 <72552136+akalayci18@users.noreply.github.com>
2020-12-24 15:02:15 +0300

    Notif-iOS

---
+++ Assets/Scripts/NotificaitonIOS.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_IOS
+    using Unity.Notifications.iOS;
+#endif
+
+public class Notifications : MonoBehaviour
+{
+#if UNITY_IOS
+    private string notificationId = "Notification Title";
+    private int identifier;
+
+    void Start()
+    {
+        var timeTrigger = new iOSNotificationTimeIntervalTrigger()
+        {
+            TimeInterval = new TimeSpan(0, 0, 10),
+            Repeats = false
+        };
+
+    }
+
+    void OnApplicationPause(bool isPause)
+    {
+
+
+        if (isPause)
+        {
+
+            System.DateTime dateNotify = DateTime.Now.AddSeconds(30);
+
+            iOSNotification notification = new iOSNotification()
+            {
+                Identifier = "notification",
+                Title = "PATİ!",
+                Subtitle = "A test notification",
+                Body = "Minik doslarımızın sana her zaman ihtiyaçları olduğunu unutma!",
+                ShowInForeground = true,
+                ForegroundPresentationOption = (PresentationOption.Alert | PresentationOption.Sound),
+                CategoryIdentifier = "category_a",
+                ThreadIdentifier = "thread1",
+                Trigger = timeTrigger,
+            };
+
+            iOSNotificationCenter.ScheduleNotification(notification);
+        }
+    }
+#endif
+}
